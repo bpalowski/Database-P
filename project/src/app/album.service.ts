033725fa94c82e2aa62f5bf533caac46cc87b97d
Brian Palowski and Eddie Harris <brian@>
2018-08-22 09:08:09 -0700

    ADD change to marketplace TS

--- project/src/app/album.service.ts
+++ project/src/app/album.service.ts
@@ -22,7 +22,8 @@ export class AlbumService {
     this.albums.push(newAlbum);
   }
 
-  getAlbumById(albumId: number){
+  getAlbumById(albumId: string){
+    return this.database.object('albums/' + albumId);
     //   for (var i = 0; i <= ALBUMS.length - 1; i++) {
     //     if (ALBUMS[i].id === albumId) {
     //       return ALBUMS[i];
---
+++ project/src/app/api-keys.ts
@@ -0,0 +1,8 @@
+export const masterFirebaseConfig = {
+    apiKey: "AIzaSyCyu4WwAu64elplgUF5mvk9OZByzFi10go",
+    authDomain: "myproject-cb9c2.firebaseapp.com",
+    databaseURL: "https://myproject-cb9c2.firebaseio.com",
+    projectId: "myproject-cb9c2",
+    storageBucket: "myproject-cb9c2.appspot.com",
+    messagingSenderId: "960013543165"
+  };
